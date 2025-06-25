2ff906be65e993c73b0165fa48b287ec32a792e7
Chris Fraser <github@cfraser.sent.com>
2025-06-25 11:28:03 -0400

    Implement LaTex Math

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,28 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import questionListIntegration from './integrations/question-list.js';
 import statisticsDataIntegration from './integrations/statistics-data.js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), questionListIntegration(), statisticsDataIntegration()],
+  integrations: [
+    tailwind(), 
+    react(), 
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex]
+    }), 
+    questionListIntegration(), 
+    statisticsDataIntegration()
+  ],
   // Add metadata for better SEO and search indexing
   site: 'https://radio.frasernolet.com',
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       // Choose the theme for syntax highlighting
       theme: 'github-light'
